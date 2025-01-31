ec38d46f7fcc1959ef99d9074aa0fee13f0dfe94
SpiderMan6699 <gregmalyk@gmail.com>
2025-01-31 09:49:34 +0100

    fix build errors

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,23 +1,23 @@
 
 export const isCpf = (c) => {
-    if((c = c.replace(/[^\d]/g,"")).length != 11) return false;
-    if (c == "00000000000") return false;
+    if((c = c.replace(/[^\d]/g,"")).length !== 11) return false;
+    if (c === "00000000000") return false;
     let r;
     let s = 0;   
     for (let i=1; i<=9; i++) s = s + parseInt(c[i-1]) * (11 - i); 
     r = (s * 10) % 11;
-    if ((r == 10) || (r == 11)) r = 0;
-    if (r != parseInt(c[9])) return false;
+    if ((r === 10) || (r === 11)) r = 0;
+    if (r !== parseInt(c[9])) return false;
     s = 0;
     for (let i = 1; i <= 10; i++) s = s + parseInt(c[i-1]) * (12 - i); 
     r = (s * 10) % 11;
-    if ((r == 10) || (r == 11)) r = 0;
-    if (r != parseInt(c[10])) return false;
+    if ((r === 10) || (r === 11)) r = 0;
+    if (r !== parseInt(c[10])) return false;
     return true;
 }
 
 export const isEmail = (email) => {
-    let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 }
 
