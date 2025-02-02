25d77887410deb03489707e2d050dec66a62d8b5
SpiderMan6699 <gregmalyk@gmail.com>
2025-02-02 02:17:36 +0100

    add all cantons default + placeholder remove

--- src/components/Form/Select/index.js
+++ src/components/Form/Select/index.js
@@ -1,79 +1,80 @@
 import React, { useState } from "react";
 import PropTypes from 'prop-types';
-
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-
-import {
-    MaterialLabel,
-    MaterialSelect
-} from "./styled";
+import { MaterialSelect } from "./styled";
 import { ThemedComponent } from "ui/theme";
 
-export const Select = ({ placeholder, options, small, value, onChange, id, secondary, borderBackground, formed }) => {
+export const Select = ({ 
+  placeholder, 
+  options, 
+  small, 
+  value, 
+  onChange, 
+  id, 
+  secondary, 
+  borderBackground, 
+  formed 
+}) => {
+  const [opened, setOpened] = useState(false);
+  
+  const toggleOpen = () => {
+    setOpened(!opened);
+  };
 
-    const [opened, setOpened] = useState(false)
-
-    const toggleOpen = () => {
-        setOpened(!opened)
+  const optionAction = item => {
+    if (onChange && typeof onChange === 'function') {
+      onChange(item.target.value);
     }
+    toggleOpen();
+  };
 
-    const optionAction = item => {
-        if (onChange && typeof onChange === 'function') {
-            onChange(item.target.value)
-        }
-        toggleOpen()
-    }
-
-    return (
-        <>
-            <ThemedComponent>
-                <FormControl variant="outlined" fullWidth
-                >
-                    <MaterialLabel id={id}
-                        color={secondary ? 'secondary' : 'white'} >{placeholder}</MaterialLabel>
-                    <MaterialSelect
-                        small={small}
-                        labelId={id}
-                        color={secondary ? 'secondary' : 'background'}
-                        borderBackground={borderBackground}
-                        id={`select-${id}`}
-                        value={value}
-                        onChange={optionAction}
-                        label={placeholder}
-                        formed={formed}
-                    >
-                        {
-                            options?.map((item, key) =>
-                                <MenuItem key={key} value={`${item.id}`}>{item?.title}</MenuItem>
-                            )
-                        }
-                    </MaterialSelect>
-                </FormControl>
-            </ThemedComponent>
-        </>
-    )
-}
-
+  return (
+    <ThemedComponent>
+      <FormControl variant="outlined" fullWidth>
+        <MaterialSelect
+          small={small}
+          displayEmpty
+          color={secondary ? 'secondary' : 'background'}
+          borderBackground={borderBackground}
+          id={`select-${id}`}
+          value={value}
+          onChange={optionAction}
+          formed={formed}
+          renderValue={(selected) => {
+            if (!selected) {
+              return <span>{placeholder}</span>;
+            }
+            return options.find(option => `${option.id}` === selected)?.title;
+          }}
+        >
+          {options?.map((item, key) => (
+            <MenuItem key={key} value={`${item.id}`}>
+              {item?.title}
+            </MenuItem>
+          ))}
+        </MaterialSelect>
+      </FormControl>
+    </ThemedComponent>
+  );
+};
 
 Select.propTypes = {
-    placeholder: PropTypes.string,
-    options: PropTypes.array,
-    value: PropTypes.object,
-    small: PropTypes.bool,
-    secondary: PropTypes.bool,
-    onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  options: PropTypes.array,
+  value: PropTypes.string,
+  small: PropTypes.bool,
+  secondary: PropTypes.bool,
+  onChange: PropTypes.func.isRequired,
 };
-
 
 Select.defaultProps = {
-    placeholder: '',
-    options: [],
-    value: undefined,
-    small: false,
-    secondary: false,
-    onChange: undefined,
+  placeholder: '',
+  options: [],
+  value: '',
+  small: false,
+  secondary: false,
+  onChange: undefined,
 };
 
-export default Select; 
+export default Select;
--- src/components/HomeFilters/index.js
+++ src/components/HomeFilters/index.js
@@ -1,8 +1,7 @@
-import React, { useContext, useMemo, useRef, useState, useEffect } from 'react'
+import React, { useContext, useMemo, useRef, useState } from 'react'
 import { FiltersContainer, FilterTitle, FormContainer } from './styled'
 import Button from 'components/Form/Button'
 import FormCore from '../../components/Form/Core'
-import { Container } from 'reactstrap'
 import { CoreContext } from 'context/CoreContext'
 import { optionsCategory } from 'utils/options'
 import useI18n from 'hooks/useI18n'
@@ -11,7 +10,7 @@ export default function HomeFilters() {
   const { setModal, regions, cities, services, filter, setFilter, escorts } = useContext(CoreContext)
   const { t } = useI18n()
   const [changed, setChanged] = useState(false)
-  const [nextPrint, setNextPrint] = useState([])
+  const [selectedRegion, setSelectedRegion] = useState('all')
   const formRef = useRef()
 
   const formItems = useMemo(() => {
@@ -20,23 +19,44 @@ export default function HomeFilters() {
     const prices = escorts?.map((m) => m?.profile?.prices)?.reduce((p, c) => [...p, ...(c||[])] , [])
       ?.reduce((p, c) => p?.includes(c?.text) ? p : [...p, c?.text] , [])?.filter(f => f)?.map(m => ({ id:m, title:m }))
 
+    const allRegionsOptions = [
+      { id: 'all', title: t('All Cantons') },
+      ...(regions || []).filter(r => r.id !== 'all')
+    ]
+
     return [
       {
         ref: 'region',
         placeholder: t('All Cantons'),
-        options: regions,
+        options: allRegionsOptions,
         customer: true,
-        onBlur: () => {
+        defaultValue: 'all',
+        onChange: (value) => {
+          setSelectedRegion(value)
+          const form = formRef?.current?.getForm()
+          if (form) {
+            form.city = ''
+            if (value === 'all') {
+              delete form.region
+            } else {
+              form.region = value
+            }
+          }
           setChanged(!changed)
         }
-      }, 
+      },
       {
         ref: 'city',
         placeholder: t('ville'),
-        options: form?.region ? cities?.filter(f => `${f?.region?.data?.id}` === `${form?.region}`) : [],
+        options: selectedRegion === 'all' ? [] : 
+          cities?.filter(f => `${f?.region?.data?.id}` === `${selectedRegion}`),
         customer: true,
-        disabled: !form?.region,
-        onBlur: () => setChanged(!changed)
+        disabled: selectedRegion === 'all',
+        style: selectedRegion === 'all' ? { 
+          opacity: 0.5,
+          pointerEvents: 'none',
+          backgroundColor: 'rgba(255, 255, 255, 0.1)'
+        } : {}
       },
       {
         ref: 'service',
@@ -65,47 +85,30 @@ export default function HomeFilters() {
         action: () => save(),
       },
     ].filter(f => f)
-  }, [regions, services, escorts, cities, filter, changed, t])
+  }, [regions, services, escorts, cities, filter, changed, selectedRegion, t])
 
   const save = () => {
     const form = formRef?.current?.getForm()
-    setFilter({ ...form })
+    if (form && form.region === 'all') {
+      const { region, ...restForm } = form
+      setFilter(restForm)
+    } else {
+      setFilter({ ...form })
+    }
     window.scrollTo(0, 720)
   }
 
-  useEffect(() => {
-    setNextPrint(formItems)
-  }, [formItems])
-
-  useEffect(() => {
-    const currentForm = formRef?.current?.getForm()
-    if (currentForm?.region) {
-      const updatedFormItems = nextPrint.map(item => {
-        if (item.ref === 'city') {
-          return {
-            ...item,
-            disabled: false
-          }
-        }
-        return item
-      })
-      setNextPrint(updatedFormItems)
-    }
-  }, [formRef?.current?.getForm()?.region, nextPrint])
-
   return (
-    <>
-      <FiltersContainer>
-        <FilterTitle>
-          { t('find_the_ideal_model') }
-        </FilterTitle>
-        <FormContainer>
-          <FormCore ref={formRef} register={filter} formItems={formItems} />
-          <Button outlineGradient nospace onClick={() => setModal({ type: 'searchadvanced' })}>
-            <strong>{ t('advanced_search') }</strong>
-          </Button>
-        </FormContainer>
-      </FiltersContainer>
-    </>
+    <FiltersContainer>
+      <FilterTitle>
+        {t('find_the_ideal_model')}
+      </FilterTitle>
+      <FormContainer>
+        <FormCore ref={formRef} register={filter} formItems={formItems} />
+        <Button outlineGradient nospace onClick={() => setModal({ type: 'searchadvanced' })}>
+          <strong>{t('advanced_search')}</strong>
+        </Button>
+      </FormContainer>
+    </FiltersContainer>
   )
 }
