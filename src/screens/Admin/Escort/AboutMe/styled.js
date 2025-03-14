4f3a1b9b25254ecdc14433e82aaf68bf08a70562
SpiderMan6699 <gregmalyk@gmail.com>
2025-03-14 06:00:53 +0100

    abtmeFIx

--- src/screens/Admin/Escort/AboutMe/styled.js
+++ src/screens/Admin/Escort/AboutMe/styled.js
@@ -5,25 +5,31 @@ export const BodyContainer = styled.div.attrs({})`
   min-height: 100%;
   padding-top: 120px;
   margin-top: 90px;
-  background: linear-gradient(to left, ${p => p.theme.palette.gradient.secondary} 0%, ${p => p.theme.palette.gradient.primary} 100%);
   padding-top: 1px;
+  
   @media(max-width:767px){
-    margin-right: 0px;
-    max-width: calc(100% - 60px);
+    margin-right: 0;
+    max-width: 100%;
+    padding: 80px 16px 0;
   }
 `;
 
 export const BodyContent = styled.div.attrs({})`
   width: 100%;
   height: 100%;
-  background: ${p => p.theme.palette.colors.black};
   padding: 37px 0 62px 0;
   
   @media(max-width:767px){
-    padding: 0 12px;
+    padding: 12px 0;
+    margin-bottom: 70px;
   }
 `;
 
 export const AdminContainer = styled.div.attrs({})`
- display: flex;
+  display: flex;
+  width: 100%;
+  
+  @media(max-width:767px){
+    justify-content: center;
+  }
 `;
