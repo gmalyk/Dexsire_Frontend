6c415ef488b9e1f5fec2867aad36e923d370977a
SpiderMan6699 <gregmalyk@gmail.com>
2025-03-17 16:30:28 +0100

    fix desktop admin header

--- src/components/Admin/Header/index.js
+++ src/components/Admin/Header/index.js
@@ -6,7 +6,9 @@ import {
   HeaderContent, 
   HeaderTopSection,
   TitleContainer,
-  GradientLine
+  GradientLine,
+  DesktopHeader,
+  MobileHeader
 } from './styled'
 import { Icon, Title } from 'ui/styled'
 import Input from 'components/Form/Input'
@@ -16,40 +18,59 @@ import { useHistory } from 'react-router-dom'
 import useI18n from 'hooks/useI18n'
 
 export default function AdminHeader({ title }) {
-
   const history = useHistory(); 
-  const navigate = to => history.push(`/${ to }`); 
-
+  const navigate = to => history.push(`/${to}`); 
   const { adminPages } = useContext(CoreContext)
-
   const { t } = useI18n()
-
   const [form, setForm] = useState({})
   const formValue = ref => { return form?.[ref] ? form?.[ref] : ''; }
   const changeForm = (value, ref) => { setForm({ ...form, [ref]: value }); }
  
   return (
     <>
-      <HeaderContainer>
-        <HeaderTopSection>
-          <Icon icon="logo" width={137} nomargin /> 
-          <HeaderButtonContainer>
-            <HeaderButton>
-              <Icon icon="notification" />
-            </HeaderButton>
-            <HeaderButton orange>
-              <Icon icon="setting" />
-            </HeaderButton> 
-          </HeaderButtonContainer>
-        </HeaderTopSection>
-        
-        <HeaderContent>
-          <TitleContainer>
-            <Title  nomargin>{title}</Title>
-            <GradientLine />
-          </TitleContainer>
-        </HeaderContent>
-      </HeaderContainer>
+      {/* Desktop Header (new layout) */}
+      <DesktopHeader>
+        <HeaderContainer>
+          <HeaderContent>
+            <Title small nomargin>{title}</Title>
+            <HeaderButtonContainer>
+              <Select 
+                placeholder={t("what_are_you_looking_for")} 
+                options={adminPages} 
+                noHolder 
+                outline 
+                endIcon={'search'}
+                value={formValue('search')} 
+                onChange={link => navigate(link)} 
+              />
+            </HeaderButtonContainer>
+          </HeaderContent>
+        </HeaderContainer>
+      </DesktopHeader>
+
+      {/* Mobile Header (original layout) */}
+      <MobileHeader>
+        <HeaderContainer>
+          <HeaderTopSection>
+            <Icon icon="logo" width={137} nomargin /> 
+            <HeaderButtonContainer>
+              <HeaderButton>
+                <Icon icon="notification" />
+              </HeaderButton>
+              <HeaderButton orange>
+                <Icon icon="setting" />
+              </HeaderButton> 
+            </HeaderButtonContainer>
+          </HeaderTopSection>
+          
+          <HeaderContent>
+            <TitleContainer>
+              <Title nomargin>{title}</Title>
+              <GradientLine />
+            </TitleContainer>
+          </HeaderContent>
+        </HeaderContainer>
+      </MobileHeader>
     </>
   )
 }
--- src/components/Admin/Header/styled.js
+++ src/components/Admin/Header/styled.js
@@ -1,16 +1,46 @@
 import styled from "styled-components";
 
+// Base container that will be used by both mobile and desktop
 export const HeaderContainer = styled.div.attrs({})`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  padding: 32px 40px 0 0;
-  position: absolute;
   width: 100%;
-  top: 0;
+`;
+
+// Desktop-specific container (shown only on desktop)
+export const DesktopHeader = styled.div`
+  display: none;
   
-  @media(max-width:767px){
+  @media(min-width: 768px) {
+    display: block;
+    width: 100%;
+    
+    ${HeaderContainer} {
+      justify-content: flex-end;
+      align-items: center;
+      padding: 32px 40px 40px 0;
+      position: absolute;
+      width: 100%;
+      top: 0;
+      flex-direction: row;
+    }
+  }
+`;
+
+// Mobile-specific container (shown only on mobile)
+export const MobileHeader = styled.div`
+  display: block;
+  
+  @media(min-width: 768px) {
+    display: none;
+  }
+  
+  ${HeaderContainer} {
     padding: 32px 12px 0 12px;
+    position: absolute;
+    width: 100%;
+    top: 0;
   }
 `;
 
@@ -22,14 +52,18 @@ export const HeaderTopSection = styled.div.attrs({})`
   margin-bottom: 14px;
 `;
 
+// Different styling for desktop and mobile
 export const HeaderContent = styled.div.attrs({})`
   display: flex;
   flex-direction: column;
   width: 100%;
   gap: 16px;
   
-  @media(max-width:767px){
-    width: 100%;
+  @media(min-width: 768px) {
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: calc(100% - 293px);
   }
 `;
 
@@ -39,7 +73,7 @@ export const TitleContainer = styled.div.attrs({})`
   width: 100%;
   padding-left: 16px;
   
-  @media(max-width:767px){
+  @media(max-width: 767px) {
     padding-left: 0;
   }
 `;
@@ -51,9 +85,15 @@ export const GradientLine = styled.div.attrs({})`
   background: linear-gradient(to right, ${p => p.theme.palette.gradient.primary} 0%, ${p => p.theme.palette.gradient.secondary} 100%);
 `;
 
+// Different styling for desktop and mobile
 export const HeaderButtonContainer = styled.div.attrs({})`
   display: flex;
   gap: 18px;
+  
+  @media(min-width: 768px) {
+    width: 100%;
+    max-width: 568px;
+  }
 `;
 
 export const HeaderButton = styled.div.attrs({})`
